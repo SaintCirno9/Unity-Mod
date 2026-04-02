e79e0f810fea809b56f572bc70de1fc6c369bd16
Can <zby03772014@hotamil.com>
2026-04-02 17:28:34 +0800

    update 3.4.1

--- LongYinLiZhiZhuan/LYMod/Main.cs
+++ LongYinLiZhiZhuan/LYMod/Main.cs
@@ -14,7 +14,7 @@ namespace LYMod;
 public static class ModConstants
 {
     public const string ModName = "LYMod";     // 插件名
-    public const string ModVersion = "3.4";    // 版本号
+    public const string ModVersion = "3.4.1";    // 版本号
     public const string ModAuthor = "Can";     // 作者
 }
 
